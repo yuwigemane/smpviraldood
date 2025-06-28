be2448582428c902fe5c6188f971aedfdfadbe09
yuwigemane <yuwigemane@gmail.com>
2025-06-28 20:57:30 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'Smp Viral Dood';
-export const desk = 'Video bokep indo jepang viral ngentot colmek bocil sampai tante.';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Gb Sma Cindo Tante China Terbaru Viral';
 export const url = 'https://smpviraldood.pages.dev';
-export const terbit = '2025-06-23T00:00:00Z';
+export const terbit = '2025-06-23T00:01:03Z';
